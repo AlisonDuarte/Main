15df8f9c8edeb7e6de4908c0c4ff000d7c43d89c
AlisonDuarte <dru.rodrigo@outlook.com>
2022-08-25 22:00:22 -0300

    Initial commit

---
+++ app/src/main/java/br/com/etecia/main/Componentes.java
@@ -0,0 +1,14 @@
+package br.com.etecia.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Componentes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.componentes_layout);
+    }
+}
